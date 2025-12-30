c33d64f8c8578767f384871af1210be392fcac43
sai kiran <saikiranmedisetti2003@gmail.com>
2025-12-30 18:20:51 +0530

    commit

---
+++ ComcastCRMGUIFramework/src/test/java/com/crm/comcast/orgtest/CreateContactMvn.java
@@ -0,0 +1,19 @@
+package com.crm.comcast.orgtest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactMvn {
+	   @Test
+	     public void  createContact() {
+		   
+		    String s1=   System.getProperty("url");
+		   String s2=    System.getProperty("name");
+		   System.out.println(s1);
+		   System.out.println(s2);
+		   
+		       
+		   
+		   
+	   }
+
+}
