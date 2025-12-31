be37c0e3cdec0debc43093643f4bbdd5ba284135
sai kiran <saikiranmedisetti2003@gmail.com>
2025-12-31 09:50:21 +0530

    commited

---
+++ ComcastCRMGUIFramework/src/test/java/com/crm/comcast/contactTest/ContactJenkins.java
@@ -0,0 +1,22 @@
+package com.crm.comcast.contactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactJenkins {
+	    
+	  @Test
+	  public void Contact() {
+		  
+		  String s1=   System.getProperty("url");
+		   String s2=    System.getProperty("name");
+		   System.out.println(s1);
+		   System.out.println(s2);
+		  
+		  
+		  
+		  
+		  
+		  
+	  }
+
+}
